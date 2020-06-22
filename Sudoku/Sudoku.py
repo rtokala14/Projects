d3f7aa46d1552529818501223003032b3c6698e8
Rohit Reddy Tokala <rtokala@purdue.edu>
2020-06-22 16:04:22 -0400

    Build Board

--- Sudoku/Sudoku.py
+++ Sudoku/Sudoku.py
@@ -1,5 +1,22 @@
-import numpy as np
 
-Board = [[0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9, [0]*9]
 
-print(Board[2][3])
+
+# Load the board
+def loadBoard():
+    Board = []
+
+    fileHandle = open("Board.txt", "r")
+    data = fileHandle.readlines()
+    for line in range(len(data)):
+        if line != len(data) - 1:
+            data[line] = data[line][:-1]
+        Board.append(list(map(int, data[line].split(","))))
+    fileHandle.close()
+
+    return Board
+
+Board = loadBoard()
+for x in range(len(Board)):
+    for y in range(len(Board[0])):
+        print(Board[x][y], end=" ")
+    print()
